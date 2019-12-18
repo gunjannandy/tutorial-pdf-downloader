34aff3bf591edc9e75992ea611f5f4bed2cd083f
Gunjan <gunjan.nandy1@gmail.com>
2019-12-18 18:52:56 +0530

    Updated links

--- download_links.py
+++ download_links.py
@@ -1,6 +1,6 @@
 download_list = {
-	"https://www.javatpoint.com/aptitude/quantitative" ,
+	# "https://www.javatpoint.com/aptitude/quantitative" ,
 	"https://www.tutorialspoint.com/react_native/react_native_alert.htm" ,
 	"https://www.javatpoint.com/apache-ant-tutorial" ,
-	"https://www.tutorialspoint.com/java/index.htm" ,
+	# "https://www.tutorialspoint.com/java/index.htm" ,
 }
